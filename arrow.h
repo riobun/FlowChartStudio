598198f2128e452f3beef2052b9e090fd7569c3c
1853582 <642335434@qq.com>
2020-07-19 17:53:12 +0800

    实现了箭头的折线绘制，但存在bug

--- arrow.h
+++ arrow.h
@@ -1,11 +1,49 @@
+
 #ifndef ARROW_H
 #define ARROW_H
+#include<QStyle>
+#include <QGraphicsLineItem>
 
+class DiagramItem;
 
-class Arrow
+//! [0]
+class Arrow : public QGraphicsLineItem
 {
 public:
-    Arrow();
+    enum { Type = UserType + 4 };
+
+    Arrow(DiagramItem *startItem, DiagramItem *endItem,
+          QGraphicsItem *parent = nullptr);
+
+    int type() const override { return Type; }
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void setColor(const QColor &color) { myColor = color; }
+    void setType(int flag);
+    void setSize(int size);
+    void setId(int size);
+    void setList();
+    DiagramItem *startItem() const { return myStartItem; }
+    DiagramItem *endItem() const { return myEndItem; }
+
+    void updatePosition();
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+               QWidget *widget = nullptr) override;
+
+
+
+private:
+    DiagramItem *myStartItem;
+    DiagramItem *myEndItem;
+    QPolygonF arrowHead;
+    QColor myColor = Qt::black;
+    int id=1;
+    int atype=1;
+    int asize=2;
+    QList<QPointF> list;
 };
+//! [0]
 
 #endif // ARROW_H
