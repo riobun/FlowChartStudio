3cf1a15435b887c621a384e2bd05e96ab233ec0b
1853582 <642335434@qq.com>
2020-07-19 20:19:02 +0800

    GraphElement的一个初始化

--- graphelement.cpp
+++ graphelement.cpp
@@ -1,6 +1,14 @@
 #include "graphelement.h"
+#include "diagramitem.h"
 
+#include <QPainter>
+#include <QPen>
+#include <QtMath>
+#include<QMouseEvent>
 GraphElement::GraphElement()
 {
 
+}
+void GraphElement::setId(){
+    this->Id=k++;
 }
--- graphelement.h
+++ graphelement.h
@@ -1,11 +1,23 @@
 #ifndef GRAPHELEMENT_H
 #define GRAPHELEMENT_H
+#include "arrow.h"
 
-
-class GraphElement
+//class GraphElement
+//{
+//public:
+//    GraphElement();
+//};
+int k=0;
+class GraphElement //图形元素
 {
-public:
-    GraphElement();
-};
+      public:
+          GraphElement();
+          ~GraphElement();
+          GraphElement* father;
+          int Id;//单个文件内唯一,文件内可能存在若干子图，但ID不同
+//		  Text& content; 文本内容
+          void setId();
 
+
+};
 #endif // GRAPHELEMENT_H
