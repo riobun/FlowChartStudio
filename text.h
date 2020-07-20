fa43a6a969af5e5722e1a9cb4f5114c2617fd187
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-07-20 22:56:45 +0800

    修改了text头文件

--- text.h
+++ text.h
@@ -1,30 +1,42 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+
 #include <QMainWindow>
 #include<QString>
+#include<QFont>
+#include<QGraphicsRectItem>
+#include<QGraphicsScene>
+#include <QGraphicsTextItem>
+#include<QColor>
 
-
-class Text{
+class Text {
 public:
-
-    Text(QPoint primary_location,double primary_width,double primary_hight);//新建文本框，参数：节点或鼠标位置，节点大小
+    Text( QPoint primary_location, double primary_width, double primary_height);//由节点生成
+    Text(QPointF position1, QPointF position2);//由图生成（鼠标位置决定大小）
+    ~Text();
+    void build_text(QGraphicsScene* scene,QColor c,QFont f);//初始化并在界面中显示，注：创建文本框时一定要调用
     void delete_text();
-    void delete_char(int begin,int end);//参数：待删除字符位置
-    void add_char(int position,QString c);//增加字符位置，增加的字符
-    void revise_font(QFont new_font);//新字体
-    Text(Text old_t,QPoint primary_location,double primary_width,double primary_hight);//附着于新的节点
-    void move_text(QPoint new_location);//参数：新的坐标位置
+    void delete_char(int begin, int end);//参数：待删除字符位置
+    void add_char(int position, QString str);//增加字符位置，增加的字符
+    void reset_font(QFont new_font);//新字体
+    void reset_color(QColor new_color);
+    Text(Text old_t, QPointF primary_location, double primary_width, double primary_height);//附着于新的节点
+    void move_text(QPointF new_location);//参数：新的坐标位置
 
     QFont get_text_fond();
     QString get_text_content();
-    QPoint get_text_location();
+    QPointF get_text_location();
+    QColor get_text_color();
 
 private:
-    QPoint location;
+    QPointF location;
     QString content;
     QFont font;
-    double width,hight;
+    double width, height;
+    QColor color;
+    QGraphicsRectItem* item = new QGraphicsRectItem();
+    QGraphicsTextItem* text = new QGraphicsTextItem(item);
 };
 
 
