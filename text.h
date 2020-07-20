36c57723f3ae9aefffce167bdf26bc4d9cd0a1d8
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-07-20 16:16:58 +0800

    Update text.h

--- text.h
+++ text.h
@@ -10,9 +10,8 @@ public:
 
     Text(QPoint primary_location,double primary_width,double primary_hight);//新建文本框，参数：节点或鼠标位置，节点大小
     void delete_text();
-    void delete_char(int position);//参数：待删除字符位置
-    void change_char(int position,char c);//待替换字符位置，新字符
-    void add_char(int position,char c);//增加字符位置，增加的字符
+    void delete_char(int begin,int end);//参数：待删除字符位置
+    void add_char(int position,QString c);//增加字符位置，增加的字符
     void revise_font(QFont new_font);//新字体
     Text(Text old_t,QPoint primary_location,double primary_width,double primary_hight);//附着于新的节点
     void move_text(QPoint new_location);//参数：新的坐标位置
