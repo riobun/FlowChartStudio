6bb9d48590bf730136525c4c9a7cd80054a16ec8
ShaRuichen <1747308644@qq.com>
2020-07-20 10:04:33 +0800

    Revert "GraphElement的一个初始化"
    This reverts commit 3cf1a15435b887c621a384e2bd05e96ab233ec0b.

--- graphelement.cpp
+++ graphelement.cpp
@@ -1,14 +1,6 @@
 #include "graphelement.h"
-#include "diagramitem.h"
 
-#include <QPainter>
-#include <QPen>
-#include <QtMath>
-#include<QMouseEvent>
 GraphElement::GraphElement()
 {
 
-}
-void GraphElement::setId(){
-    this->Id=k++;
 }
--- graphelement.h
+++ graphelement.h
@@ -1,23 +1,11 @@
 #ifndef GRAPHELEMENT_H
 #define GRAPHELEMENT_H
-#include "arrow.h"
 
-//class GraphElement
-//{
-//public:
-//    GraphElement();
-//};
-int k=0;
-class GraphElement //图形元素
+
+class GraphElement
 {
-      public:
-          GraphElement();
-          ~GraphElement();
-          GraphElement* father;
-          int Id;//单个文件内唯一,文件内可能存在若干子图，但ID不同
-//		  Text& content; 文本内容
-          void setId();
-
-
+public:
+    GraphElement();
 };
+
 #endif // GRAPHELEMENT_H
