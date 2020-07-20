905de8c726155f2c65ad95e9d60c86601260b9ab
riobun <49815645+riobun@users.noreply.github.com>
2020-07-20 23:50:54 +0800

    美化2.0，添加更多的控件，完善了菜单栏和工具栏

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include <QToolBar>
 #include <QPushButton>
 #include <QComboBox>
+#include <QColorDialog>
+#include <QFontComboBox>
+#include <QFontDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,15 +14,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    //工具栏
     ui->toolBar->addWidget(ui->backBtn);
     ui->toolBar->addWidget(ui->copyBtn);
     ui->toolBar->addWidget(ui->pasteBtn);
 
     ui->toolBar->addSeparator();
 
-    QComboBox* fontBtn = new QComboBox(this);
-    fontBtn->addItem("宋体");
-    fontBtn->addItem("黑体");
+    QFontComboBox* fontBtn = new QFontComboBox(this);
     ui->toolBar->addWidget(fontBtn);
 
     ui->toolBar->addSeparator();
@@ -35,6 +37,51 @@ MainWindow::MainWindow(QWidget *parent)
     fontColorBtn->addItem("black");
     fontColorBtn->addItem("blue");
     ui->toolBar->addWidget(fontColorBtn);
+
+    ui->toolBar->addSeparator();
+
+     ui->toolBar->addWidget(ui->boldBtn);
+     ui->toolBar->addWidget(ui->italicBtn);
+     ui->toolBar->addWidget(ui->underlineBtn);
+
+
+    //侧边栏
+    connect(ui->bgColorBtn,&QPushButton::clicked,[](){
+        QColorDialog::getColor(QColor(Qt::white));
+    });
+    connect(ui->bdColorBtn,&QPushButton::clicked,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+    connect(ui->arrowColorBtn,&QPushButton::clicked,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+    ui->arrowComboBox->addItem("实线");
+    ui->arrowComboBox->addItem("虚线");
+
+
+
+
+    //菜单栏信号
+    //文本框
+    connect(ui->action_fontColor,&QAction::triggered,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+    connect(ui->action_font,&QAction::triggered,[](){
+        bool flag;
+        QFontDialog::getFont(&flag,QFont("宋体",20));
+    });
+    //箭头
+    connect(ui->action_arrowColor,&QAction::triggered,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+    //节点
+    connect(ui->action_bgColor,&QAction::triggered,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+    connect(ui->action_bdColor,&QAction::triggered,[](){
+        QColorDialog::getColor(QColor(Qt::black));
+    });
+
 
     _scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene());
