3faa1bef9384b3c06e01380b3fd1f2fe5869d872
ShaRuichen <1747308644@qq.com>
2020-07-21 10:49:29 +0800

    添加graph类

--- graph.h
+++ graph.h
@@ -1,7 +1,21 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include"node.h"
+#include"text.h"
 
 
+class Graph:public GraphElement
+{
+public:
+    Graph();
+private:
+    bool isExpended;
+    Node& subGraphNode;
+    QMap<int, Node&> nodes;
+    QMap<int, Arrow&> Arrows;
+    QMap<int, Text&> texts;
+    QMap<int, Graph&> subGraph;
+};
 
 #endif // GRAPH_H
