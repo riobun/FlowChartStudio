0fa4b00cc7c418d7b29d8cda1ba59cb3f298936a
ShaRuichen <1747308644@qq.com>
2020-07-22 17:00:46 +0800

    实现拖动框。

--- flowchartscene.cpp
+++ flowchartscene.cpp
@@ -9,3 +9,43 @@ void FlowChartScene::keyPressEvent(QKeyEvent *event)
 {
 
 }
+
+void FlowChartScene::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    QGraphicsScene::mousePressEvent(event);
+    if (!event->isAccepted())
+    {
+        if (event->button() == Qt::MouseButton::LeftButton)
+        {
+            selectLeftTop = event->scenePos();
+            rect = new Rectangle(event->scenePos(), 1, 1);
+            rect->Paint(this);
+        }
+    }
+}
+
+void FlowChartScene::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    QGraphicsScene::mouseMoveEvent(event);
+    if (rect)
+    {
+        QPointF size = event->scenePos() - selectLeftTop;
+        QPointF position = selectLeftTop + size / 2;
+        if (size.rx() < 0) size.rx() = -size.rx();
+        if (size.ry() < 0) size.ry() = -size.ry();
+        rect->SetWidth(size.rx());
+        rect->SetHeight(size.ry());
+        rect->SetLocation(position - selectLeftTop / 2);
+    }
+}
+
+void FlowChartScene::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    QGraphicsScene::mouseReleaseEvent(event);
+    if (rect)
+    {
+
+        rect->Remove(this);
+        rect = nullptr;
+    }
+}
--- flowchartscene.h
+++ flowchartscene.h
@@ -11,6 +11,9 @@ public:
 
 protected:
     void keyPressEvent(QKeyEvent *event) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
 };
 
 #endif // FLOWCHARTSCENE_H
