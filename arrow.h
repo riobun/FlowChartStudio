84ba0f3b718db536a4bde87f7180a686b173844c
1853582 <642335434@qq.com>
2020-07-22 15:56:02 +0800

    添加接口

--- arrow.h
+++ arrow.h
@@ -1,11 +1,51 @@
-#ifndef ARROW_H
-#define ARROW_H
-#include "graphelement.h"
 
-class Arrow:public GraphElement
+#ifndef NEWARROW_H
+#define NEWARROW_H
+class Node;
+#include "graphelement.h"
+#include <QPainter>
+#include <QPen>
+#include <QtMath>
+#include<QMouseEvent>
+class Arrow: public QGraphicsPathItem,public GraphElement
 {
 public:
-    Arrow();
+    enum { Type = UserType + 4 };
+
+    Arrow(Node *startItem, Node *endItem,
+          QGraphicsItem *parent = nullptr);
+
+    int type() const override { return Type; }
+    QRectF boundingRect() const override;
+    QPainterPath shape() const override;
+    void setColor(const QColor &color) { myColor = color; }
+    void setType(int flag);
+    void setSize(int size);
+    void setId(int size);
+    void setList();
+    QPolygonF polygon();
+    Node *startItem() const { return myStartItem; }
+    Node *endItem() const { return myEndItem; }
+
+    void updatePosition();
+
+protected:
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+               QWidget *widget = nullptr) override;
+
+
+
+private:
+    Node *myStartItem;
+    Node *myEndItem;
+    QPolygonF arrowHead;
+    QList<QLineF> lines;
+    QColor myColor = Qt::black;
+    int id=1;
+    int atype=1;
+    int asize=2;
+    QList<QPointF> list;// 箭头结点
+    QPainterPath *apath;//箭头路径
 };
 
-#endif // ARROW_H
+#endif // NEWARROW_H
--- graphelement.cpp
+++ graphelement.cpp
@@ -1,11 +1,26 @@
 #include "graphelement.h"
-
+//#include "diagramitem.h"
+#include <QPainter>
+#include <QPen>
+#include <QtMath>
+#include<QMouseEvent>
+//extern int k1=0;
+//int GraphElement::xy[50][50]={0};
 GraphElement::GraphElement()
 {
-
+    for(int i=0;i<100;i++){
+        k1[i][0]=i+1;
+        k1[i][1]=0;
+    }
 }
-
-int GraphElement::GetID()
-{
-    return id;
+void GraphElement::setId(){
+    for(int i=0;i<100;i++){
+        if(k1[i][1]==0){
+            this->Id=i+1;
+            k1[i][1]=0;
+        }
+    }
+}
+int GraphElement::GetID(){
+    return this->Id;
 }
