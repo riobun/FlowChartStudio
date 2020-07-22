45b06dcd06da94924d2a9a8015b2a5dae3746685
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-07-22 10:24:11 +0800

    添加文本框的点击事件

--- textitem.cpp
+++ textitem.cpp
@@ -1,9 +1,24 @@
 #include<textitem.h>
-    TextItem ::TextItem(){}
+#include<textevent.h>
+    /*TextItem ::TextItem(){}
     void  TextItem ::keyPressEvent(QKeyEvent* event) override{}
     void  TextItem ::mousePressEvent(QGraphicsSceneMouseEvent* event)override{}
     void  TextItem ::mouseMoveEvent(QGraphicsSceneMouseEvent* event)override{}
     void  TextItem ::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)override{}
     void  TextItem ::mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event)override{}
     void  TextItem ::keyReleaseEvent(QKeyEvent* event)override{}
-    void  TextItem ::contextMenuEvent(QGraphicsSceneContextMenuEvent* event)override{}
+    void  TextItem ::contextMenuEvent(QGraphicsSceneContextMenuEvent* event)override{}*/
+void TextItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    NodeEvents::mousePressEvent(text, event);
+}
+
+void TextItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    NodeEvents::mouseReleaseEvent(text, event);
+}
+
+void TextItem::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+{
+    NodeEvents::contextMenuEvent(text, event);
+}
