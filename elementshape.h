560f8de760fe6cf7faf1735cbeab562e22126547
ShaRuichen <1747308644@qq.com>
2020-08-06 19:04:02 +0800

    修复添加箭头节点的bug。

--- elementshape.h
+++ elementshape.h
@@ -21,7 +21,7 @@ inline bool isNode(ElementShape shape)
     return shape == ElementShape::Input || shape == ElementShape::Output ||
             shape == ElementShape::Diamond || shape == ElementShape::SubGraph ||
             shape == ElementShape::Rectangle || shape == ElementShape::InnerInput ||
-            shape == ElementShape::InnerOutput;
+            shape == ElementShape::InnerOutput || shape == ElementShape::Arrownode;
 }
 
 #endif // SHAPE_H
