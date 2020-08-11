a9e86231c63ef7e79634a01c56472c66d9b99763
ShaRuichen <1747308644@qq.com>
2020-08-11 17:06:22 +0800

    修复bug。

--- changeelementaction.cpp
+++ changeelementaction.cpp
@@ -31,6 +31,7 @@ void ChangeElementAction::Do()
 {
     auto window = MainWindow::instance();
     auto scene = ::scene ? ::scene : window->scene();
+    auto graph = scene->graph;
     if (isNode(shape))
     {
         auto node = static_cast<Node*>(element);
@@ -40,8 +41,8 @@ void ChangeElementAction::Do()
             connect(item, &NodeItem::Selected, this, &ChangeElementAction::onNodeSelected);
             connect(item, &NodeItem::NewLocation, this, &ChangeElementAction::onNodeMoved);
             node->Paint(scene);
-            MainWindow::instance()->graph()->addNode(node);
-            MainWindow::instance()->graph()->addNode(node,MainWindow::instance()->index_tab(),shape);
+            graph->addNode(node);
+            graph->addNode(node,MainWindow::instance()->index_tab(),shape);
         }
         else
         {
@@ -53,7 +54,7 @@ void ChangeElementAction::Do()
                 action->Do();
                }
             node->Remove(scene);
-            MainWindow::instance()->graph()->removeNode(node);
+            graph->removeNode(node);
             MainWindow::instance()->selectedNodes()->remove(node->GetID());
             disconnect(node->getNodeItem(), &NodeItem::Selected, this, &ChangeElementAction::onNodeSelected);
             node->getNodeItem()->setSelected(false);
@@ -66,7 +67,7 @@ void ChangeElementAction::Do()
         {
             auto parent = text->parent;
             if (parent) parent->content = text;
-            window->graph()->addText(text);
+            graph->addText(text);
             text->putup_text(scene);
             connect(text, &Text::Selected, this, &ChangeElementAction::onTextSelected);
             text->build_text(text->get_text_color(), text->get_text_font());
@@ -75,7 +76,7 @@ void ChangeElementAction::Do()
         {
             auto parent = text->parent;
             if (parent) parent->content = nullptr;
-            window->graph()->removeText(text);
+            graph->removeText(text);
             window->selectedTexts()->removeAll(text);
             text->delete_text(scene);
             disconnect(text, &Text::Selected, this, &ChangeElementAction::onTextSelected);
@@ -91,16 +92,16 @@ void ChangeElementAction::Do()
             arrow->myEndItem->GetNode()->ConnectAsDestination(arrow);
             arrow->setZValue(-100.0);
             arrow->s = onArrowSelected;
-            MainWindow::instance()->scene()->addItem(arrow);
+            scene->addItem(arrow);
             arrow->updatePosition();
-            MainWindow::instance()->graph()->addArrow(arrow);
+            graph->addArrow(arrow);
         }
         else
         {
             arrow->myStartItem->GetNode()->DisconnectAsSource(arrow);
             arrow->myEndItem->GetNode()->DisconnectAsDestination(arrow);
-            MainWindow::instance()->graph()->removeArrow(arrow);
-            MainWindow::instance()->scene()->removeItem(arrow);
+            graph->removeArrow(arrow);
+            scene->removeItem(arrow);
             MainWindow::instance()->selectedArrows()->remove(arrow->GetID());
             arrow->s = nullptr;
             arrow->setSelected(false);
